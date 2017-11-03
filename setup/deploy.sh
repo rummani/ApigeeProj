f4b751d0cfd737d67a53fafe6ced6a837fa8fc23
Ravi <rummani@prokarma.com>
2017-11-03 11:23:46 -0600

    added code Fix

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./setenv.sh
+source ../setenv.sh
 
 echo "Enter directory name for sample proxy to be deployed, followed by [ENTER]:"
 
@@ -12,10 +12,8 @@ read -s password
 
 echo Deploying $api_Name to $env on $url using $username and $org
 
-../tools/deploy.py -n $api_Name -u $username:$password -o $org -h $url -e $env -p / -d ../$api_Name -h $url
+../tools/deploy.py -n $api_Name -u $username:$password -o $org -h $url -e $env -p / -d ./$api_Name -h $url
 
 echo "If 'State: deployed', then your API Proxy is ready to be invoked."
 
 echo "Change directory to the sample API proxy deployed, and run 'invoke.sh'"
-
-./provisioning_for_oauth.sh $proxy
