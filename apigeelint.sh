475fa740f81a4be533acce510f6a3d8974780e6e
rummani <33236365+rummani@users.noreply.github.com>
2017-11-08 15:50:19 -0700

    Update apigeelint.sh

--- apigeelint.sh
+++ apigeelint.sh
@@ -1,3 +1,3 @@
  node -v
  npm install -g apigeelint
- ./node_modules/apigeelint/main.js -s ./streaming/apiproxy/ -f compact.js
+ ./node_modules/apigeelint/main.js -s ./streaming/apiproxy/ -f html.js
