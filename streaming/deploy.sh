eb3a717e873f2ffe26f35be714e3dd21396d3ef0
rummani <33236365+rummani@users.noreply.github.com>
2017-11-08 13:15:06 -0700

    Update deploy.sh

--- streaming/deploy.sh
+++ streaming/deploy.sh
@@ -5,7 +5,7 @@ source ../../setup/setenv.sh
 echo "Enter your password for the Apigee Enterprise organization $org, followed by [ENTER]:"
 
 read -s password
-apigeelint -s ../streaming/apiproxy / -f table.js
+$ apigeelint -s ../streaming/apiproxy / -f table.js
 
 echo Deploying $proxy to $env on $url using $username and $org
 ../../tools/deploy.py -n streaming -u $username:$password -o $org -h $url -e $env -p / -d ../streaming
