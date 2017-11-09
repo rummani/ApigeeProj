8cf8c70ddbde40a4669d67c5c86a6ad1a64ba905
rummani <33236365+rummani@users.noreply.github.com>
2017-11-09 14:58:29 -0700

    Update test.js

--- Test/test.js
+++ Test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var test = require('./streaming/apiproxy/resources/jsc/calc.js');
+var test = require('C:/Program Files (x86)/Jenkins/workspace/JenkinsProject/streaming/apiproxy/resources/jsc/calc.js');
 describe("calcTest",function(){
   describe('addTest', function() {
     it('it should give valid result', function(){
