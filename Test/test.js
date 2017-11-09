8c2081469f3e9abff3cd83c83049c01ff9350d82
Ravi <rummani@prokarma.com>
2017-11-09 13:56:15 -0700

    added Junit scripts

---
+++ Test/test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var test = require('./calc.js');
+describe("calcTest",function(){
+  describe('addTest', function() {
+    it('it should give valid result', function(){
+      assert.equal(5, test.checkSum(2,3));
+    });
+  });
+
+  describe('multiTest', function() {
+    it('it should give valid result', function(){
+      assert.equal(6, test.checkMulti(2,3));
+    });
+  });
+
+  describe('divTest', function() {
+    it('it should give valid result', function(){
+      assert.equal(2, test.checkDiv(6,3));
+    });
+  });
+});
---
+++ streaming/apiproxy/resources/jsc/calc.js
@@ -0,0 +1,13 @@
+var convert ={};
+
+convert.checkSum=function(p1,p2) {
+    return p1+p2;
+}
+convert.checkMulti=function(p1,p2) {
+    return p1*p2;
+}
+convert.checkDiv=function(p1,p2) {
+    return p1/p2;
+}
+
+module.exports=convert;
