38524eefe32514fb8e3878aea42e8444564d9646
rummani <33236365+rummani@users.noreply.github.com>
2017-11-09 09:43:47 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ echo "Enter your password for the Apigee Enterprise organization $org, followed 
 read -s password
 
 echo Deploying $api_name to $env on $url using $username and $org
+pwd
 ./tools/deploy.py -n $api_name -u $username:$password -o $org -h $url -e $env -p / -d ../$api_name
 
 echo "If 'State: deployed', then your API Proxy is ready to be invoked."
