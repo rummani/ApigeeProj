259ea15fb197c683ced1ded3c494fa21e5ee5460
rummani <33236365+rummani@users.noreply.github.com>
2017-11-09 10:27:36 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Enter your password for the Apigee Enterprise organization $org, followed 
 
 read -s password
 
-echo Deploying $api_name to $env on $url using $username and $org
+echo Deploying $api_name to $env on $url using $username and $org and $pwd
 pwd
 ./tools/deploy.py -n $api_name -u $username:$pwd -o $org -h $url -e $env -p / -d ../$api_name
 
