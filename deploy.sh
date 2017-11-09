0c66a86ece1d12ae74d551b675cde1adc537a8d2
Ravi <rummani@prokarma.com>
2017-11-09 09:35:08 -0700

    updated deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,9 @@ source ../../setup/setenv.sh
 echo "Enter your password for the Apigee Enterprise organization $org, followed by [ENTER]:"
 
 read -s password
-$ apigeelint -s ../streaming/apiproxy / -f table.js
 
-echo Deploying $proxy to $env on $url using $username and $org
-../../tools/deploy.py -n streaming -u $username:$password -o $org -h $url -e $env -p / -d ../streaming
+echo Deploying $api_name to $env on $url using $username and $org
+../../tools/deploy.py -n $api_name -u $username:$password -o $org -h $url -e $env -p / -d ../$api_name
 
 echo "If 'State: deployed', then your API Proxy is ready to be invoked."
 
