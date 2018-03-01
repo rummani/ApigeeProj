a25a509d94023f3c1941d5ab2982f133c51faa10
rummani <33236365+rummani@users.noreply.github.com>
2018-03-01 10:34:41 -0700

    Update apigeelint.sh

--- apigeelint.sh
+++ apigeelint.sh
@@ -1,4 +1,5 @@
  node -v
  pwd
  npm install -g apigeelint
- ./node_modules/apigeelint -s ./streaming/apiproxy/ -f table.js
+ cd ./node_modules/apigeelint
+ apigeelint -s ./streaming/apiproxy/ -f table.js
